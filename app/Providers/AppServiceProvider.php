356d376f62975de59850d71e08dae10c9c2b019f
krs91tk <krs91tk@gmail.com>
2022-06-01 18:24:29 +0900

    文字の長さ修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,26 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+
+  use Illuminate\Support\Facades\Schema;
+
+  public function boot()
+  {
+    Schema::defaultStringLength(191);
+  }
 }
