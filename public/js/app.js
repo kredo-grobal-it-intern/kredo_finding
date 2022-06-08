100fc8f0d3bd9bb0c2850875de5e73c578adb6c9
daiki1002 <ile.imeh.s37@gmail.com>
2022-06-08 19:56:38 +0900

    hide user is liked.

--- public/js/app.js
+++ public/js/app.js
@@ -2525,8 +2525,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "5afc47f86cd3b783adf2",
-  cluster: "ap3",
+  key: "",
+  cluster: "mt1",
   encrypted: true
 });
 
