90631c2f4667106da985218db53655dcfddc3ebd
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-06-10 19:47:22 +0900

    add create new account button

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,6 +6,8 @@
       <div class="loginPage_contents">
         <h1 class="h3 loginPage_contents_title">Let's find new colleague</h1>
         <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">Log in with email</a></div>
+        <div class="btn loginPage_contents_btn bg-white border"><a class="text-primary" href="{{ route('login') }}"><i class="fa-brands fa-facebook"></i>Log in with Facebook</a></div>
+        <div class="btn loginPage_contents_btn bg-white border"><a class="text-dark" href="{{ route('register') }}">Create New Account</a></div>
       </div>
     </div>
   </div>
