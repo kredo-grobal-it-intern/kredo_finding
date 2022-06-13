8bacfe4fe283faa88c754e689eaa484a739e240a
sarasara73 <sarasara73.slrs@gmail.com>
2022-06-13 22:25:04 +0900

    adding the top image

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
   <div class="loginPage">
-    <div class="container">
-      <div class="loginPage_contents">
-        <h1 class="h3 loginPage_contents_title">Let's find new colleague</h1>
-        <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">Log in with email</a></div>
-      </div>
+    <div class="loginPage_title">
+      <h1 class="h3 ">Let's find new colleague</h1>
     </div>
+    <div class="topVideo">
+      <video src="/storage/videos/pexels-yan-krukov-7692917.mp4" autoplay muted></video>
+    </div>
+    <div class="btn loginPage_btn"><a class="text-white" href="{{ route('login') }}">Log in with email</a></div>
   </div>
 @endsection
