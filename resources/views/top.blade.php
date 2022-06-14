99e65c5956425e2ad43e42aae0ddd65198e223be
sarasara73 <sarasara73.slrs@gmail.com>
2022-06-14 19:46:39 +0900

    fixed the top image

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,7 +6,7 @@
       <h1 class="h3 ">Let's find new colleague</h1>
     </div>
     <div class="topVideo">
-      <video src="/storage/videos/pexels-yan-krukov-7692917.mp4" autoplay muted></video>
+      <video src="/videos/pexels-yan-krukov-7692917.mp4" autoplay muted></video>
     </div>
     <div class="btn loginPage_btn"><a class="text-white" href="{{ route('login') }}">Log in with email</a></div>
   </div>
