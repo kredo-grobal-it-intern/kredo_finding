9ed7fc1d0d5758678e43105d102b90dcef8ea939
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-06-16 20:50:13 +0900

    add facebook button

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
    * @var array
    */
   protected $fillable = [
-    'name', 'email', 'password', 'self_introduction', 'gender', 'img_name', 'google_id'
+    'name', 'email', 'password', 'self_introduction', 'gender', 'img_name', 'google_id', 'facebook_id',
   ];
 
   /**
