adbcdcee52c352787b998c67c932ab27c0c23494
daiki1002 <ile.imeh.s37@gmail.com>
2022-06-18 16:46:14 +0900

    title changed

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="loginPage">
     <div class="loginPage_title">
-      <h1 class="h3 ">Let's find new colleague</h1>
+      <h1 class="h3 ">Let's find new friend</h1>
     </div>
     <div class="topVideo">
       <video src="/videos/pexels-yan-krukov-7692917.mp4" autoplay muted></video>
