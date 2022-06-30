871ba3b8cebe42e211cde445e3abc7948ac63212
daiki1002 <ile.imeh.s37@gmail.com>
2022-06-30 19:55:35 +0900

    add company user

--- resources/views/companies/register.blade.php
+++ resources/views/companies/register.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
-  <div class="signupPage">
+  <div class="signupCompanyPage">
     <header class="header">
       <div>Create new account</div>
     </header>
     <div class='container'>
-      <div class="text-right pt-2"><a href="{{ route('company.register') }}">For company</a></div>
       <form class="form mt-5" method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
         @csrf
 
-        <input type="hidden" name="user_type" value=0>
+        <input type="hidden" name="user_type" value="1">
 
         <label for="file_photo" class="rounded-circle userProfileImg">
           <div class="userProfileImg_description">Upload Profile Image</div>
@@ -24,7 +23,7 @@
         </div>
 
         <div class="form-group @error('name')has-error @enderror">
-          <label>Name</label>
+          <label>Company</label>
           <input type="text" name="name" class="form-control" value="{{ old('name') }}" placeholder="Enter your name">
           @error('name')
             <span class="errorMessage">{{ $message }}</span>
@@ -49,19 +48,8 @@
           <label>Confirmation Password</label>
           <input type="password" name="password_confirmation" class="form-control" placeholder="Enter confirmation password">
         </div>
-        <div class="form-group">
-          <div><label>Gender</label></div>
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" name="gender" value="0" type="radio" id="inlineRadio1">
-            <label class="form-check-label" for="inlineRadio1">male</label>
-          </div>
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" name="gender" value="1" type="radio" id="inlineRadio2">
-            <label class="form-check-label" for="inlineRadio2">female</label>
-          </div>
-        </div>
         <div class="form-group @error('self_introduction')has-error @enderror">
-          <label>Self Introduction</label>
+          <label>Company Info</label>
           <textarea class="form-control" name="self_introduction" rows="10">{{ old('self_introduction') }}</textarea>
           @error('self_introduction')
             <span class="errorMessage">{{ $message }}</span>
@@ -69,9 +57,6 @@
         </div>
         <div class="text-center">
           <button type="submit" class="btn submitBtn">Register</button>
-          <a href="{{ route('google.redirect') }}">
-            <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" class="d-block mx-auto mt-3">
-          </a>
           <div class="linkToLogin">
             <a href="{{ route('login') }}">Log in</a>
           </div>
