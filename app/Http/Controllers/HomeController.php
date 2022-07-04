823c52753e5e122f274def69f7f99273b4134946
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-07-05 01:05:43 +0900

    modify change password function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,12 +47,10 @@ class HomeController extends Controller
 
 public function changePasswordPost(Request $request) {
     if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
-        // The passwords matches
         return redirect()->back()->with("error","Your current password does not matches with the password.");
     }
 
     if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
-        // Current password and new password same
         return redirect()->back()->with("error","New Password cannot be same as your current password.");
     }
 
@@ -61,7 +59,6 @@ public function changePasswordPost(Request $request) {
         'new-password' => 'required|string|min:8|confirmed',
     ]);
 
-    //Change Password
     $user = Auth::user();
     $user->password = bcrypt($request->get('new-password'));
     $user->save();
