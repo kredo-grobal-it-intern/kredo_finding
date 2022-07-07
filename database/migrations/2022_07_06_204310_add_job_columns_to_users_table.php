59f06655a03d2c4e57dd08db43d93adc2fc6893d
daiki1002 <ile.imeh.s37@gmail.com>
2022-07-07 19:40:03 +0900

    delete rename columns in migration for adding job columns

--- database/migrations/2022_07_06_204310_add_job_columns_to_users_table.php
+++ database/migrations/2022_07_06_204310_add_job_columns_to_users_table.php
@@ -18,9 +18,6 @@ class AddJobColumnsToUsersTable extends Migration
             $table->string('job_skills')->nullable()->default(NULL)->after('self_introduction');
             $table->string('occupation')->nullable()->default(NULL)->after('self_introduction');
             $table->tinyInteger('is_active')->nullable(false)->default(0)->after('facebook_id')->comment('0:inactive 1:active');
-            $table->renameColumn('email_verified_at', 'date_email_verified');
-            $table->renameColumn('created_at', 'date_created');
-            $table->renameColumn('updated_at', 'date_updated');
         });
     }
 
@@ -36,9 +33,6 @@ class AddJobColumnsToUsersTable extends Migration
             $table->dropColumn('job_skills');
             $table->dropColumn('occupation');
             $table->dropColumn('is_active');
-            $table->renameColumn('date_email_verified', 'email_verified_at');
-            $table->renameColumn('date_created', 'created_at');
-            $table->renameColumn('date_updated', 'updated_at');
         });
     }
 }
