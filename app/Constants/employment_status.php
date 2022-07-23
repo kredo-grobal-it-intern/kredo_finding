bcf2a99de3483c54d672d86aa170f0515947d4d4
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-07-23 10:50:11 +0900

    add employment_status to status.php

---
+++ app/Constants/employment_status.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class Employment_status
+{
+  public const Employment_status = [
+     1 => 'Full-time',
+     2 => 'Part-time',
+     3 => 'contract-employee',
+  ];
+}
