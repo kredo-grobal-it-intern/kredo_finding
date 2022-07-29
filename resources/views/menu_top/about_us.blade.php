2385a3b3b9da70fff3c0d6b454afd790b7e4872a
passionEgg <1228yanagi@gmail.com>
2022-07-29 20:37:26 +0900

    add link to logo on About Us page and Contact Us page

--- resources/views/menu_top/about_us.blade.php
+++ resources/views/menu_top/about_us.blade.php
@@ -5,20 +5,21 @@
 <div class="container-fluid">
     <div class="container-fluid mt-2">
         <header class="row">
-            <div class="col-1"><img src="/images/kredo_logo.jpg"  width="50px" height="50px"></div>
-            <div class="col-11 text-center ABOUTUS">ABOUT US</div> 
+
+            <div class="col-1"> <a href="{{URL::to('/')}}"><img src="/images/kredo_logo.jpg"  width="50px" height="50px"></div>
+            <div class="col-11 text-center ABOUTUS">ABOUT US</div>
         </header>
     </div>
 
-    <hr> 
-        
-    <div class="container-fluid AboutusPage"> 
+    <hr>
+
+    <div class="container-fluid AboutusPage">
         <div class="row">
             <div class="col-5 AboutusPic2">
             </div>
 
             <div class="col-7 mt-5">
-                <h2 class="title text-center mt-3 ">OUR VISION</h2> 
+                <h2 class="title text-center mt-3 ">OUR VISION</h2>
                     <p class="lead text-secondary mt-3">Provide a friendly and effective system for companies and individuals in expanding their network in terms of work or personal connection</p>
 
                 <h2 class="title text-center mt-5">OUR MISSION</h2>
@@ -28,7 +29,7 @@
                     <p class="text-white h3">LEARN MORE</p>
                 </button>
             </div>
-        </div> 
+        </div>
     </div>
 
 </div>
