332aa17f0b45ff4023a00141ba9b90b512bdb7ae
Yuya Takahashi <ahiru.3128@gmail.com>
2022-08-04 21:47:40 +0900

    design user&company detail page

--- resources/views/mypage/liked_company_detail.blade.php
+++ resources/views/mypage/liked_company_detail.blade.php
@@ -1,27 +1,35 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
-  <div class="header text-center mt-5">
-    <h1>Company Details</h1>
-  </div>
-  <div class="card bg-light w-75 mx-auto mt-3">
+<div class="container company_detail">
+  <div class="card bg-light w-75 mx-auto mt-5">
     <div class="card-header border border-none">
-      <div class="name text-center">
-        {{ $company->name }}
+      <div class="company_img">
+        <div class="name text-white">
+          <p class="h2">{{ $company->name }}</p>
+        </div>
       </div>
     </div>
     <div class="card-body bg-white">
-      <div class="email">
-        <p>
-          Email:&nbsp;{{ $company->email }}
-        </p>
-      </div>
-      <div class="self_introduction">
-        <p>
-          Self Introduction:&nbsp;{{ $company->self_introduction }}
-        </p>
-      </div>
+      <h3 class="text-info">Company Overview</h3>
+      <table class="table border">
+        <thead>
+          <tr>
+            <th class="bg-info text-center text-white">Company Name</th>
+            <th>{{ $company->name }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="bg-info text-center text-white">Email</td>
+            <td>{{ $company->email }}</td>
+          </tr>
+          <tr>
+            <td class="bg-info text-center text-white">Self Introduction</td>
+            <td>{{ $company->self_introduction }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </div>
--- resources/views/mypage/liked_user_detail.blade.php
+++ resources/views/mypage/liked_user_detail.blade.php
@@ -1,32 +1,40 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
-  <div class="header text-center mt-5">
-    <h1>User Details</h1>
-  </div>
-  <div class="card bg-light w-75 mx-auto mt-3">
-    <div class="card-header border border-none">
-      <div class="name text-center">
-        {{ $user->first_name }}&nbsp;{{ $user->last_name }}
+<div class="container user_detail">
+  <div class="card bg-light w-75 mx-auto mt-5">
+    <div class="card-header border border-none p-0">
+      <div class="user_img">
+        <div class="name text-white">
+          <p class="h2">
+            User Overview
+          </p>
+        </div>
       </div>
     </div>
     <div class="card-body bg-white">
-      <div class="gender">
-        <p>
-          Gender:&nbsp;{{ $user->gender == 1 ? 'Female' : 'Male' }}
-        </p>
-      </div>
-      <div class="email">
-        <p>
-          Email:&nbsp;{{ $user->email }}
-        </p>
-      </div>
-      <div class="self_introduction">
-        <p>
-          Self Introduction:&nbsp;{{ $user->self_introduction }}
-        </p>
-      </div>
+      <table class="table border">
+        <thead>
+          <tr>
+            <th class="bg-success text-center text-white">Name</th>
+            <th>{{ $user->first_name }}&nbsp;{{ $user->last_name }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="bg-success text-center text-white">Gender</td>
+            <td>{{ $user->gender == 1 ? 'Female' : 'Male' }}</td>
+          </tr>
+          <tr>
+            <td class="bg-success text-center text-white">Email</td>
+            <td>{{ $user->email }}</td>
+          </tr>
+          <tr>
+            <td class="bg-success text-center text-white">Self Introduction</td>
+            <td>{{ $user->self_introduction }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </div>
