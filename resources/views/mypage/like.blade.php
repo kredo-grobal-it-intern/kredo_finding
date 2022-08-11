fa8c085cf5112fca7943138a467516cb57209a40
daiki1002 <ile.imeh.s37@gmail.com>
2022-08-11 16:29:49 +0900

    no message

--- resources/views/mypage/like.blade.php
+++ resources/views/mypage/like.blade.php
@@ -21,7 +21,7 @@
             @foreach($you_liked as $you_liked_user)
             <div class="liking_wrap">
               @if(isWorker(Auth::id()))
-                <a href="#" @if($you_liked_user->companyUser->img_name) class="liking_img" @endif>{{ profileImageInLike($you_liked_user->companyUser->img_name) }}</a>
+                <a href="{{ route('company_detail.show', $you_liked_user->companyUser->id) }}" @if($you_liked_user->companyUser->img_name) class="liking_img" @endif>{{ profileImageInLike($you_liked_user->companyUser->img_name) }}</a>
                 <div class="liking_name">{{ $you_liked_user->companyUser->name.'  ['.App\Constants\Occupation::Occupation[$you_liked_user->occupation].']' }} 
                   <p class="h5 text-secondary">{{ date("m/d/Y", strtotime($you_liked_user->created_at)) }}</p>
                 </div>
@@ -56,7 +56,7 @@
             @foreach( $liked_by as $liked_by_user)
             <div class="liking_wrap">
               @if (!isWorker(Auth::id()))
-                <a href="#" @if($liked_by_user->fromWorkerId->img_name) class="liking_img" @endif>{{ profileImageInLike($liked_by_user->fromWorkerId->img_name) }}</a>
+                <a href="{{ route('user_detail.show', $liked_by_user->fromWorkerId->id) }}" @if($liked_by_user->fromWorkerId->img_name) class="liking_img" @endif>{{ profileImageInLike($liked_by_user->fromWorkerId->img_name) }}</a>
                 <div class="liking_name">{{ $liked_by_user->fromWorkerId->name }} <i class="fa-solid fa-heart text-danger" style="font-size: 0.7rem;"> {{ App\Constants\Occupation::Occupation[$liked_by_user->toJobId->occupation] }}</i>
                   <p class="h5 text-secondary">{{ date("m/d/Y", strtotime($liked_by_user->created_at))}}</p>
                 </div>
@@ -65,7 +65,7 @@
                   <input name="user_id" type="hidden" value="">
                 </form>
               @else
-                <a href="{{ route('user_detail.show', $liked_by_user->fromUserId->id) }}" @if($liked_by_user->fromUserId->img_name) class="liking_img" @endif>{{ profileImageInLike($liked_by_user->fromUserId->img_name) }}</a>
+                <a href="{{ route('company_detail.show', $liked_by_user->fromUserId->id) }}" @if($liked_by_user->fromUserId->img_name) class="liking_img" @endif>{{ profileImageInLike($liked_by_user->fromUserId->img_name) }}</a>
                 <div class="liking_name">{{ $liked_by_user->fromUserId->name }}
                   <p class="h5 text-secondary">{{ date("m/d/Y", strtotime($liked_by_user->created_at))}}</p>
                 </div>
