e356cc18a470b30fbe2388267666e97b0cc3d9e4
daiki1002 <ile.imeh.s37@gmail.com>
2022-08-13 10:42:18 +0900

    bugfix swipe and search of tenurship

--- app/Constants/Tenureship.php
+++ app/Constants/Tenureship.php
@@ -5,15 +5,15 @@ namespace App\Constants;
 class Tenureship
 {
   public const Tenureship = [
-     0 => '1 ~ less than 2',
-     1 => '2 ~ less than 3',
-     2 => '3 ~ less than 4',
-     3 => '4 ~ less than 5',
-     4 => '5 ~ less than 6',
-     5 => '6 ~ less than 7',
-     6 => '7 ~ less than 8',
-     7 => '8 ~ less than 9',
-     8 => '9 ~ less than 10',
-     9 => '10 ~',
+     1 => '1 ~ less than 2',
+     2 => '2 ~ less than 3',
+     3 => '3 ~ less than 4',
+     4 => '4 ~ less than 5',
+     5 => '5 ~ less than 6',
+     6 => '6 ~ less than 7',
+     7 => '7 ~ less than 8',
+     8 => '8 ~ less than 9',
+     9 => '9 ~ less than 10',
+     10 => '10 ~',
   ];
 }
