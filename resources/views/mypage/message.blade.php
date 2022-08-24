801e9b884a4beb8a2593a8a5f3c170c3c921eee3
hiraoka <s.private08@gmail.com>
2022-08-24 20:22:10 +0900

    redesign chat box

--- resources/views/mypage/message.blade.php
+++ resources/views/mypage/message.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-  <div class="matchingPage">
+  {{-- <div class="matchingPage">
     <div class="container">
       <div class="mt-5">
         <div class="matchingNum">You've matched with {{ $match_users_count }} people</div>
@@ -21,5 +21,214 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
+
+  <div class="messageBox">
+    <div class="users_chat">
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">is typing...</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+      <div class="chatbox_for_every_user">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="" class="user_icon">
+        </div>
+        <div class="chatbox_detail">
+          <span class="chatbox_user_name">user name</span>
+          <span class="chatbox_user_msg">Hey, what's up?</span>
+        </div>
+        <div class="chatbox_timestamp">
+          <span class="time">11:45</span>
+        </div>
+      </div>
+    </div>
+    <div class="chatbox">
+      <div class="chatbox_header">
+        <div class="chatbox_user_icon">
+          <img src="{{ asset('images/facebook.png') }}" alt="user icon" class="user_icon">
+        </div>
+        
+        <div class="chatbox_detail">
+          <p class="chatbox_header_user_name">snapchat</p>
+          <p class="chatbox_user_status">online</p>
+        </div>        
+      </div>
+      <div class="chatbox_main">
+        <div class="msg_for_you">
+          <img src="{{ asset('images/facebook.png') }}" alt="user icon" class="user_icon_for_msg">
+          <div class="message_unit_you">
+            <p class="every_message_you">Hey, what's up?</p>
+          </div>
+          <div class="message_unit_you">
+            {{-- <p class="every_message_you">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus molestias recusandae quod veritatis nostrum! Sapiente, dignissimos maxime nobis doloremque aliquid aperiam reiciendis ea dolorem aut illum cupiditate. Aut, delectus. Sapiente officiis dolorem id reiciendis pariatur mollitia ipsam delectus provident hic!</p> --}}
+          </div>
+        </div>        
+        
+        <div class="message_unit_me">
+          <span class="every_message_me">Lorue!</span>
+        </div>
+
+        <div class="msg_for_you">
+          <img src="{{ asset('images/facebook.png') }}" alt="user icon" class="user_icon_for_msg">
+          <div class="message_unit_you">
+            <p class="every_message_you">Hey, what's up?</p>
+          </div>
+          <div class="message_unit_you">
+            {{-- <p class="every_message_you">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus molestias recusandae quod veritatis nostrum! Sapiente, dignissimos maxime nobis doloremque aliquid aperiam reiciendis ea dolorem aut illum cupiditate. Aut, delectus. Sapiente officiis dolorem id reiciendis pariatur mollitia ipsam delectus provident hic!</p> --}}
+          </div>
+        </div>
+
+        <div class="message_unit_me">
+          {{-- <span class="every_message_me">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quisquam ullam unde nisi optio magni, corrupti aperiam animi beatae, tenetur harum numquam consequatur modi accusantium adipisci et incidunt soluta error dignissimos odit ea facere minus. Ea, aliquam aut id nemo consequuntur non error. Ipsam accusantium repellendus sunt placeat molestias distinctio?</span> --}}
+        </div>
+
+        
+      </div>
+      <div class="chatbox_footer">
+        <textarea name="enter_message" id="enter_message" class="enter_message" cols="30" placeholder="Type your message here..."></textarea>
+        <button type="submit" class="send_button">Send</button>
+      </div>
+    </div>
+  </div>  
+
 @endsection
