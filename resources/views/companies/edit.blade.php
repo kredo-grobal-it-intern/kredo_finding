0c989f7ac853d5bd464ef28762b3ef70a6c1c13e
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-08-24 22:18:49 +0900

    I also fix it by company side

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -13,7 +13,7 @@
     </form>
     @endif
     <div class='container'>
-      <form class="form mt-5" method="POST" action="/users/updateUser/{{ $user->id }}"
+      <form class="form mt-5" method="POST" action="/users/{{ $user->id }}"
         enctype="multipart/form-data">
         @csrf
         @method('PATCH')
