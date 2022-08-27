017b5359020a556129d6714fa8962cb3dba34821
hiraoka <s.private08@gmail.com>
2022-08-27 11:32:28 +0900

    re-design message page

--- resources/views/mypage/message.blade.php
+++ resources/views/mypage/message.blade.php
@@ -1,28 +1,6 @@
 @extends('layouts.layout')
 
 @section('content')
-  {{-- <div class="matchingPage">
-    <div class="container">
-      <div class="mt-5">
-        <div class="matchingNum">You've matched with {{ $match_users_count }} people</div>
-        <h2 class="pageTitle">List of matched people</h2>
-        <div class="matchingList">
-          @foreach( $matching_users as $user)
-            <div class="matchingPerson">
-              <a href="#" class="text-dark @if($user->img_name) matchingPerson_img @endif">{{ profileImageInMessage($user->img_name) }}</a>
-              <div class="matchingPerson_name">{{ $user->name }}</div>
-              <form method="POST" action="{{ route('chat.show') }}">
-                @csrf
-                <input name="user_id" type="hidden" value="{{ $user->id }}">
-                <button type="submit" class="chatForm_btn">Open Chat</button>
-              </form>
-            </div>
-          @endforeach
-        </div>
-      </div>
-    </div>
-  </div> --}}
-
   <div class="messageBox">
     <div class="users_chat">
       <div class="chatbox_for_every_user">
@@ -182,6 +160,7 @@
         </div>
       </div>
     </div>
+
     <div class="chatbox">
       <div class="chatbox_header">
         <div class="chatbox_user_icon">
@@ -191,10 +170,11 @@
         <div class="chatbox_detail">
           <p class="chatbox_header_user_name">snapchat</p>
           <p class="chatbox_user_status">online</p>
-        </div>        
+        </div>
       </div>
+
       <div class="chatbox_main">
-        <div class="msg_for_you">
+        <div class="msg_from_you">
           <img src="{{ asset('images/facebook.png') }}" alt="user icon" class="user_icon_for_msg">
           <div class="message you">
             <span>Hey, what's up?</span>
@@ -202,19 +182,19 @@
           <div class="message you">
             <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus molestias recusandae quod veritatis nostrum! Sapiente, dignissimos maxime nobis doloremque aliquid aperiam reiciendis ea dolorem aut illum cupiditate. Aut, delectus. Sapiente officiis dolorem id reiciendis pariatur mollitia ipsam delectus provident hic!</p>
           </div>
-        </div>        
+        </div>
         
         <div class="message me">
-          <span>Lorue!</span>
+          <span>Hi!</span>
         </div>
         <div class="message me">
           <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, recusandae! Reprehenderit aspernatur est dolore voluptatibus totam natus! Sit, adipisci? Repellat?</span>
         </div>
         <div class="message me">
-          <span>Lorue!</span>
+          <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit?</span>
         </div>
 
-        <div class="msg_for_you">
+        <div class="msg_from_you">
           <img src="{{ asset('images/facebook.png') }}" alt="user icon" class="user_icon_for_msg">
           <div class="message you">
             <span>Hey, what's up?</span>
@@ -227,14 +207,13 @@
         <div class="message me">
           <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quisquam ullam unde nisi optio magni, corrupti aperiam animi beatae, tenetur harum numquam consequatur modi accusantium adipisci et incidunt soluta error dignissimos odit ea facere minus. Ea, aliquam aut id nemo consequuntur non error. Ipsam accusantium repellendus sunt placeat molestias distinctio?</span>
         </div>
-
-        
       </div>
+      
       <div class="chatbox_footer">
         <textarea name="enter_message" id="enter_message" class="enter_message" cols="30" placeholder="Type your message here..."></textarea>
         <button type="submit" class="send_button">Send</button>
       </div>
     </div>
-  </div>  
+  </div>
 
 @endsection
