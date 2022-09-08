5885e98cc3652c9af2c6c7770adf73bcca636e42
passionEgg <1228yanagi@gmail.com>
2022-09-08 17:26:37 +0900

    re-designed liked page in profile page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -45,7 +45,7 @@
             <span class="font-weight-bold">MESSAGES</span>
           </a>
 
-          <a href="{{ route('contacts') }}" class="sideBarItem">
+          <a href="{{ route('contact.show') }}" class=" {{ request()->is('mypage/reatction/contact.show') ? 'active' : '' }} sideBarItem">
             <i class="fa-solid fa-envelope"></i>
             <span class="font-weight-bold">CONTACT</span>
           </a>
