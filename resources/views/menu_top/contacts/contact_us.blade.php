25cc1f0554270a43bde49b4193466672fc858123
mami25254 <amim425@gmail.com>
2022-09-29 20:15:55 +0900

    move some code to css file

--- resources/views/menu_top/contacts/contact_us.blade.php
+++ resources/views/menu_top/contacts/contact_us.blade.php
@@ -7,12 +7,11 @@
         <div class="row">
             <div class="col-5 px-5">
                 <a href="{{ route('home') }}">
-                    <img src="/images/kredo_logo.jpg" style="height: 2.5rem; width:2.5rem;">
+                    <img src="/images/kredo_logo.jpg" class="logo">
                 </a>
             </div>
             <div class="col px-0">
-                <p style="font-size:18px; font-weight:700; font-family:'Inter'; line-height:22px; color:#000000;">CONTACT
-                    US</p>
+                <p class="pagetitle">CONTACT US</p>
             </div>
         </div>
     </div>
@@ -22,14 +21,12 @@
     <div class="contact_content mt-5">
         <div class="row d-flex justify-content-center">
             <div class="col-md-6 offset-md-3">
-                <p
-                    style="font-weight:100; font-size:30px; line-height:36px; font-family:'Inter'; color:#000000; font-style:normal;">
-                    Have some questions?</p>
+                <p class="title">Have some questions?</p>
             </div>
         </div>
         <div class="row d-flex justify-content-center mt-5">
             <div class="col-md-3">
-                <img src="/images/mail.png" alt="height:150px; width:150px;">
+                <img src="/images/mail.png">
             </div>
             <div class="col-md-4">
                 <form action="{{ route('contact.confirm') }}" method="post">
@@ -61,9 +58,7 @@
                     <textarea name="inquiry" id="inquiry" cols="100" row="10" class="form-control contact-inquiry mb-3"
                         placeholder="Enter your question..." required>{{ old('inquiry') }}</textarea>
 
-                    <button type="submit" class="w-100 text-white border border-none"
-                        style="background-color: #FAAC64; font-size:22px; line-height:27px; font-weight:500;">SEND
-                        MESSAGE</button>
+                    <button type="submit" class="w-100 text-white border border-none submitbtn">SEND MESSAGE</button>
                 </form>
             </div>
         </div>
